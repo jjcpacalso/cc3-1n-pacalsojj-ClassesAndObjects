926a185631b5c23d638834696e45ebf016a639ef
jjcpacalso <jjcpacalso@uc-bcf.edu.ph>
2024-01-17 17:57:23 +0800

    Add Bank Account class

---
+++ ClassesAndObjects/BankAccount.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesAndObjects
+{
+    public class BankAccount
+    {
+        public string AccountNumber { get; }
+        public string Owner { get; set; }
+        public decimal Balance { get; }
+        
+        public void MakeDeposit(decimal amount, DateTime date, string note)
+        {
+
+        }
+
+        public void MakeWithdrawal(decimal amount, DateTime date, string note)
+        {
+
+        }
+    }
+}
