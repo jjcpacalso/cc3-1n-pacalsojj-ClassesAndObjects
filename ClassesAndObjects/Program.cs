204a83898629395265831e8b23653dc19cf78be9
jjcpacalso <jjcpacalso@uc-bcf.edu.ph>
2024-01-17 18:46:18 +0800

    Add checking for exceptions

--- ClassesAndObjects/Program.cs
+++ ClassesAndObjects/Program.cs
@@ -15,9 +15,30 @@ namespace ClassesAndObjects
             account.MakeDeposit(100, DateTime.Now, "Friend paid me back");
             Console.WriteLine(account.Balance);
 
+            try
+            {
+                account.MakeWithdrawal(750, DateTime.Now, "Attempt to overwithdraw.");
+            }catch(InvalidOperationException e)
+            {
+                Console.WriteLine("Exception caught trying to overdraw");
+                Console.WriteLine(e.ToString());
+            }
+
             BankAccount ninaAccount = new BankAccount("Nina", 500);
 
             Console.WriteLine($"Account {ninaAccount.AccountNumber} was created for {ninaAccount.Owner} with {ninaAccount.Balance} initial balance.");
+
+            BankAccount invalidAccount;
+            try
+            {
+                invalidAccount = new BankAccount("Invalid", -55);
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                Console.WriteLine("Exception caught creating account with negative balance.");
+                Console.WriteLine(e.Message);
+                return;
+            }
         }
         static void newMain()
         {
